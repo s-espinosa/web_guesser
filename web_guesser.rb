167442ea00fba8aeeef40c89be6d37b12c007a99
Sal Espinosa <sespinos@gmail.com>
2016-03-14 16:30:00 -0500

    Refactor to remove duplication and create new number after correct guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,25 +5,20 @@ require 'sinatra/reloader'
 @@guesses_remaining = 5
 
 get '/' do
-  if @@guesses_remaining == 0
-    guess = params["guess"]
-    cheat = params["cheat"]
-    message, background = check_guess(guess)
-    new_number
-    erb :index, :locals => {:num => @@secret_number, :message => message, :background => background, :cheat => cheat}
-  else
-    guess = params["guess"]
-    cheat = params["cheat"]
-    message, background = check_guess(guess)
-    @@guesses_remaining -= 1
-    erb :index, :locals => {:num => @@secret_number, :message => message, :background => background, :cheat => cheat}
-  end
+  guess = params["guess"]
+  cheat = params["cheat"]
+  message, background = check_guess(guess)
+
+  @@guesses_remaining == 0 ? new_number : @@guesses_remaining -= 1
+
+  erb :index, :locals => {:num => @@secret_number, :message => message, :background => background, :cheat => cheat}
 end
 
 def check_guess(guess)
   if guess.nil?
     ["", "#FFFFFF"]
   elsif guess.to_i == @@secret_number
+    new_number
     ["That's correct!", "#337733"]
   elsif guess.to_i > @@secret_number + 5
     ["That guess is way too high", "#aa3333"]
